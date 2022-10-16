d0807a08aa11f60f8817af94c62c45d5d300b668
Bill Yang <joyang12345@gmail.com>
2022-10-15 23:39:33 -0400

    add inputs

--- src/components/Display/index.js
+++ src/components/Display/index.js
@@ -1,10 +1,31 @@
 import './index.css';
 
-const Display = () => {
-    return(
-        <div>
-        </div>
-    )
+const Display = (props) => {
+    const data = props.data;
+    
+    try{
+        const strAscending = [...data].sort((a,b) => a.title > b.title ? 1:-1,);
+        // console.log(strAscending)
+        
+        return (
+            <table>
+                <tbody>
+                    {strAscending.map((book, index) => (
+                        <tr key={index}>
+                            <td>{book.title}</td>
+                            <td>{book.page}</td>
+                            <td>{book.des}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        )
+
+    } catch (err){
+        console.log(err);
+    }
+
+    
 }
 
 export default Display
--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,18 +1,48 @@
 import './index.css';
-import Display from '../Display/index.js'
+import Display from '../Display/index.js';
+import React, {useState} from 'react';
 
 const Topbar = () => {
 
-    let dataList = [{
-        name: "Bill"
-    }]
+    const [title, setTitle] = useState([""]);
+    const [page, setPage] = useState([""]);
+    const [des, setDes] = useState([""]);
+    const [list, setList] = useState([{
+        title:"",
+        page:"",
+        des:""
+    }]);
 
     function exportClick(){
-        console.log("test")
+        console.log("test");
+    }
+
+    function importClick(){
+        console.log("import");
     }
 
     function addClick(){
-        console.log("add");
+
+        if(title === '' || page === '' || des === ''){
+            alert("Fill in all boxes.");
+        } else {
+            try{
+                setList((...list) => [...title, ...page, ...des,{
+                    title: title,
+                    page: page,
+                    des: des
+                }]);
+                setTitle("");
+                setPage("");
+                setDes("");
+                console.log(list.toString)
+            } catch (err){
+                console.log(err)
+            }
+            
+        }
+
+        
     }
 
     function removeClick(){
@@ -24,11 +54,18 @@ const Topbar = () => {
             <div className = 'topBar'>
                 <div className = 'rightButtons'>
                     <button className = 'exportButton' onClick = {exportClick}>Export</button> 
+                    <button className = 'importButton' onClick = {importClick}>Import</button>
+
                     <button className = 'remButton' onClick = {removeClick}>Remove</button>
                     <button className = 'addButton' onClick = {addClick}>Add</button>
+
+                    <input className = 'inputBox' type = "text" placeholder='Description' value = {des} onChange={(text) => setDes(text.target.value)}/>
+                    <input className = 'inputBox' type = "text" placeholder='Page' value = {page} onChange={(text) => setPage(text.target.value)}/>
+                    <input className = 'inputBox' type = "text" placeholder='Title' value = {title} onChange={(text) => setTitle(text.target.value)}/>
+                    
                 </div>
             </div>
-            <Display dataList = {dataList}></Display>
+            <Display data = {list}></Display>
         </div>
         
         
